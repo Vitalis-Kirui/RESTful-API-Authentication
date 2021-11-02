b905d132fe18175ff6ce17498372ee43c7060e5e
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-11-02 19:29:30 +0300

    adding rest framework auth class in settings

--- calorie/settings.py
+++ calorie/settings.py
@@ -86,6 +86,16 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSIONS_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
