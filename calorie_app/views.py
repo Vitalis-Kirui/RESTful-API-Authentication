d59c4e8dc823aed6eb99c37f23a0a961a9f34c1b
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-11-03 11:49:40 +0300

    adding permision class to restricted view function

--- calorie_app/views.py
+++ calorie_app/views.py
@@ -6,5 +6,6 @@ from rest_framework.response import Response
 
 # Create your views here.
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def restricted(request, *args, **kwargs):
-    return Response(data = "Only logged in users", status=status.HTTP_200_OK)
+    return Response(data = "You are seeing this because you are logged in.", status=status.HTTP_200_OK)
