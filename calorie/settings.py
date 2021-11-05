b3ccd1a7588aa050bc3f6e1e718efb32ea12b6be
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-11-05 07:40:50 +0300

    setting up media settings

--- calorie/settings.py
+++ calorie/settings.py
@@ -165,3 +165,15 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# configuring the location for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
